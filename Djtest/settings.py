fbfcc8a924f1acf9fa7627eababc1dd9877a6e7a
stronginasuit <76620622+stronginasuit@users.noreply.github.com>
2021-11-01 17:57:54 +0100

    Update settings.py

--- Djtest/settings.py
+++ Djtest/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'cm+!oa@p!sgryi-z7igihxoz-nsp!5=q%vyz%3v6&k-t(=lhu9'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
